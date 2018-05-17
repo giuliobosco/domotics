ca3cbed46cdb9300a3035681a53bdc59f16b1c75
Giulio Bosco <giuliobva@gmail.com>
2018-05-17 14:03:19 +0200

    inserted corrected mysql connection credentials

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost:3036');
+define('DB_SERVER', '127.0.0.1:3306');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'rootpassword');
-define('DB_DATABASE', 'database');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'db_domotics');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
