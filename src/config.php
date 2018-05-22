dd841fe200dc9141b9b8ee210f96377d6eaa97d7
Giulio Bosco <giuliobva@gmail.com>
2018-05-22 09:35:46 +0200

    fix MySQL configuration (config.php)

--- src/config.php
+++ src/config.php
@@ -1,7 +1,15 @@
 <?php
-define('DB_SERVER', '127.0.0.1:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'db_domotics');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+if (!$db) {
+	echo "Error: Unable to connect to MySQL." . PHP_EOL;
+	echo "<br>Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+	echo "<br>Debugging error: " . mysqli_connect_error() . PHP_EOL;
+	exit;
+}
+
 ?>
