fc4ecbafdf76a7a7dbc6797e0a20d8352cfcc6d9
Giulio Bosco <giuliobva@gmail.com>
2018-05-22 09:37:48 +0200

    fix session check process

--- src/session.php
+++ src/session.php
@@ -4,11 +4,11 @@ session_start();
 
 $user_check = $_SESSION['login_user'];
 
-$ses_sql = mysqli_query($db,"select username from admin where username = '$user_check' ");
+$ses_sql = mysqli_query($db,"SELECT user.id, user.name FROM db_domotics.user WHERE user.username = '$user_check' ");
 
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 
-$login_session = $row['username'];
+$login_session = $row['name'];
 
 if(!isset($_SESSION['login_user'])){
 	header("location:login.php");
