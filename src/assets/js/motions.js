3ef60a9861944b9fd9aadda7fb0a59c683b9851c
win-giulio\giuliobosco <giuliobva@gmail.com>
2018-06-05 13:35:38 +0200

    fix copy - fix copyright comments & description comments

--- src/assets/js/motions.js
+++ src/assets/js/motions.js
@@ -1,24 +1,39 @@
 /*
  * Copyright 2018 Giulio Bosco (giuliobva@gmail.com)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 /*
- * project: <project-name> ([<project-git-url>])
- * description: <project-description>
+ * project: Domotics (https://github.com/giuliobosco/domotics.git)
+ * description: Home made domotics with control by web interface and arduino.
  *
  * author: Giulio Bosco (giuliobva@gmail.com)
  * version: 1.0
- * date: <date>
+ * date: 05.06.2018
  *
- * file: app.js
+ * file: motion.js
  */
 
+/**
+ * Username CSS Property Display status.
+ * @type {number}
+ */
 var usernameDisplay = 0;
 
+/**
+ * Username toggle CSS Property Display status.
+ * @param obj Username Input.
+ */
 function toggleUsername(obj) {
 	var value = obj.value;
 	if (value.length == 0 && usernameDisplay == 1) {
@@ -31,7 +46,16 @@ function toggleUsername(obj) {
 	usernameDisplay = value.length;
 }
 
+/**
+ * Password CSS Property Display status.
+ * @type {number}
+ */
 var passwordDisplay = 0;
+
+/**
+ * Password toggle CSS Property Display status.
+ * @param obj
+ */
 function togglePassword(obj) {
 	var value = obj.value;
 	if (value.length == 0 && passwordDisplay == 1) {
--- src/login.php
+++ src/login.php
@@ -1,4 +1,29 @@
 <?php
+/*
+ * Copyright 2018 Giulio Bosco (giuliobva@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*
+ * project: <project-name> (https://github.com/giuliobosco/domotics.git)
+ * description: Home made domotics with control by web interface and arduino.
+ *
+ * author: Giulio Bosco (giuliobva@gmail.com)
+ * version: 1.0
+ * date: 29.05.2018
+ *
+ * file: login.php
+ */
 include("config.php");
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -31,9 +56,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 
 <head>
+	<meta charset="UTF-8">
+	<!--
+	  -- Copyright 2018 Giulio Bosco (giuliobva@gmail.com)
+	  --
+	  -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+	  -- documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+	  -- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+	  -- and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+	  -- The above copyright notice and this permission notice shall be included in all copies or substantial portions
+	  -- of the Software.
+	  --
+	  -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+	  -- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+	  -- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+	  -- CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	  -- DEALINGS IN THE SOFTWARE.
+	  -->
+	
+	<!--
+	  -- project: <project-name> (https://github.com/giuliobosco/domotics.git)
+      -- description: Home made domotics with control by web interface and arduino.
+	  --
+	  -- author: Giulio Bosco (giuliobva@gmail.com)
+	  -- version: 1.0
+	  -- date: 05.06.2018
+	  --
+	  -- file: login.php
+	  -->
+	
+	<meta property="og:type" content="website">
+	<meta name="keywords" content="<keywords>">
+	<meta property="og:title" content="<title>">
+	<meta name="description" content="<description>">
+	<meta property="og:description" content="<descriptio>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
 	<title>Domotics</title>
 
-	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="lib/font/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="lib/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
