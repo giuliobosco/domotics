c1c87d9efc014061c282407a337625e8a2dcfdaa
giuliobosco <giuliobva@gmail.com>
2019-03-01 14:18:09 +0100

    add portal LogoutServlet

---
+++ src/portal/authentication/LogoutServlet.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package portal.authentication;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * Logout from portal servlet.
+ *
+ * @author giuliobosco (giuliobva@gmail.com)
+ * @version 1.0 (2019-02-28)
+ */
+@WebServlet(name = "LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+
+    /**
+     * On request logout, invalidate session if exists.
+     *
+     * @param request Http logout request.
+     * @param response Http logout response.
+     * @throws ServletException Error while processing the servlet.
+     * @throws IOException Error on the system.
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("JSESSIONID")){
+                    System.out.println("JSESSIONID="+cookie.getValue());
+                }
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+        // invalidate the session if exists
+        HttpSession session = request.getSession(false);
+        System.out.println("User="+session.getAttribute("user"));
+        if(session != null){
+            session.invalidate();
+        }
+        //no encoding because we have invalidated the session
+        response.sendRedirect("login.html");
+    }
+
+    /**
+     * redirect to doPost method.
+     *
+     * @param request Http request.
+     * @param response Http response.
+     * @throws ServletException Error while processing the servlet.
+     * @throws IOException Error on the system.
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
