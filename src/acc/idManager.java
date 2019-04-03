07ca067e34c183a2a77c5a5ec7524ee674d2062d
giuliobosco <giuliobva@gmail.com>
2019-04-03 16:20:27 +0200

    fix acc IdManager - class name

--- src/acc/idManager.java
+++ src/acc/idManager.java
@@ -3,8 +3,8 @@ import java.sql.*;
  *
  * @author guebe
  */
-public class idManager {
-    
+public class IdManager {
+
     /**
      * This method connects to the database
      * @param user User to log in
@@ -12,49 +12,49 @@ public class idManager {
      * @param databaseURL Url of the database e.g.:"jdbc:msql://localhost:3306/Demo"
      * @return the connection to the database
      * @throws SQLException
-     * @throws ClassNotFoundException 
+     * @throws ClassNotFoundException
      */
     public Connection connectDB(String user, String pass, String databaseURL) throws SQLException, ClassNotFoundException{
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(databaseURL, user, pass);
         return conn;
     }
-    
+
     /**
      * This method does a query on the database
      * @param c Connection to the database
      * @param query Query to execute
      * @return Returns the query
-     * @throws SQLException 
+     * @throws SQLException
      */
     public ResultSet query(Connection c, String query) throws SQLException{
         // create the java statement
         Statement st = c.createStatement();
-      
+
         // execute the query, and get a java resultset
         ResultSet rs = st.executeQuery(query);
         st.close();
-      
+
         return rs;
     }
-  
+
     /**
      * This method does an update on the database
      * @param c Connection to the database
      * @param update Update to execute
-     * @throws SQLException 
+     * @throws SQLException
      */
     public void update(Connection c, String update) throws SQLException{
         Statement st = c.createStatement();
         st.executeUpdate(update);
     }
-    
+
     /**
      * This method controls if the arduiino's ip is changed
      * @param c Connection to the database
      * @param id Arduino's id
      * @param ip Arduino's ip
-     * @throws SQLException 
+     * @throws SQLException
      */
     public void checkIp(Connection c, String id, String ip) throws SQLException{
         ResultSet rs = query(c, "SELECT ip from arduino where client_id="+id);
@@ -62,13 +62,13 @@ public class idManager {
             update(c,"update arduino set ip ="+ip+" where client_id="+id);
         }
     }
-    
+
     /**
      * This method checks if the key exists, if it doesn't exists it creates one
      * @param c Connection to the database
      * @param id Arduino's id
      * @return returns the key
-     * @throws SQLException 
+     * @throws SQLException
      */
     public String checkKey(Connection c, String id) throws SQLException{
         ResultSet rs = query(c, "SELECT ip from arduino where client_id="+id);
@@ -89,7 +89,7 @@ public class idManager {
         }
         return key;
     }
-    
+
     /**
      * This method creates a random hexadecimal key for the Arduino
      * @return Hexadecimal key
