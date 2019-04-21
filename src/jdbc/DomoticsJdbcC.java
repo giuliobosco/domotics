4671902758c3bd8a022c9e06564381d4fc7153f1
giuliobosco <giuliobva@gmail.com>
2019-04-21 23:08:21 +0200

    add acc.DomoticsJdbC

---
+++ src/jdbc/DomoticsJdbcC.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2019 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+package jdbc;
+
+/**
+ * Get the JDBC connector for domotics database.
+ *
+ * @author giuliobosco (giuliobva@gmail.com)
+ * @version 1.0 (2019-04-21)
+ */
+public class DomoticsJdbcC {
+
+    /**
+     * Domotics database username.
+     */
+    private static final String USERNAME = "root";
+
+    /**
+     * Domotics database password.
+     */
+    private static final String PASSWORD = "123qwe";
+
+    /**
+     * Domotics database host.
+     */
+    private static final String HOSTNAME = "localhost";
+
+    /**
+     * Domotics database port.
+     */
+    private static final int PORT = 3306;
+
+    /**
+     * Domotics database name.
+     */
+    private static final String DATABASE = "domotics";
+
+    /**
+     * Ge the JDBC Connector for domotics database.
+     *
+     * @return JDBC Connector for domotics database.
+     */
+    public static JdbcConnector getConnector() {
+        return new JdbcConnector(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE);
+    }
+    
+}
