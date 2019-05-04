f7e7f2cbf4706c9716ee799a002601c0fbd6172f
giuliobosco <giuliobva@gmail.com>
2019-05-04 14:51:34 +0200

    update web/lib

--- web/lib/js/contact_me.js
+++ web/lib/js/contact_me.js
@@ -1,22 +1,3 @@
-/*
- * Copyright 2018 Giulio Bosco (giuliobva@gmail.com)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-/*
- * project: contact-form (https://bitbuket.org/jdg-team/contact-form.git)
- * description: Send e-mails in web page in a form. Tipical use for contact pages.
- *
- * author: Giulio Bosco (giuliobva@gmail.com)
- * version: 1.0
- * date: 15/03/2018
- *
- * file: lib/js/contact_me.js
- */
-
 $(function() {
 
     $("input,textarea").jqBootstrapValidation({
@@ -36,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././php/contact_me.php",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
