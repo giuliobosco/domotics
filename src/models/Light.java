82863f5540871a933368b9df58c695446352815e
giuliobosco <giuliobva@gmail.com>
2019-05-08 15:19:05 +0200

    fix models.Light - getJsonString() exceptions

--- src/models/Light.java
+++ src/models/Light.java
@@ -38,7 +38,7 @@ import java.sql.SQLException;
  * Domotics Light.
  *
  * @author giuliobosco (giuliobva@gmail.com)
- * @version 1.3 (2019-04-05 - 2019-05-08)
+ * @version 1.3.1 (2019-04-05 - 2019-05-08)
  */
 public class Light {
     // ------------------------------------------------------------------------------------ Costants
@@ -168,7 +168,7 @@ public class Light {
      *
      * @return Light as JSON string.
      */
-    public String getJsonString() {
+    public String getJsonString() throws IOException {
         return getJson().toString();
     }
 
