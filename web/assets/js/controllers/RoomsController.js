17e36877ed70ffa0c0b948c175168c6786a5b9b3
giuliobosco <giuliobva@gmail.com>
2019-05-09 12:48:23 +0200

    fix web RoomsController - data.logout

--- web/assets/js/controllers/RoomsController.js
+++ web/assets/js/controllers/RoomsController.js
@@ -1,10 +1,14 @@
 /**
  * @author giuliobosco
- * @version 1.0 (2019-05-04 - 2019-05-04)
+ * @version 1.0.1 (2019-05-04 - 2019-05-08)
  */
 
 app.controller('RoomsController', ['$scope', '$sce', 'RoomsService', function($scope, $sce, newsService) {
 	newsService.then(function(data) {
+		if (data.logout && data.logout == 'logout') {
+			window.location = "index.html#!/login";
+		}
+
 		$scope.rooms = data.rooms;
 	});
 }]);
