389cd5c5a2e475b6050b9ec55ae14c6a518cff54
giuliobosco <giuliobva@gmail.com>
2019-05-09 18:53:53 +0200

    fix web RoomsController - autoreload

--- web/assets/js/controllers/RoomsController.js
+++ web/assets/js/controllers/RoomsController.js
@@ -1,14 +1,20 @@
 /**
  * @author giuliobosco
- * @version 1.0.1 (2019-05-04 - 2019-05-08)
+ * @version 1.1 (2019-05-04 - 2019-05-09)
  */
 
-app.controller('RoomsController', ['$scope', '$sce', 'RoomsService', function($scope, $sce, newsService) {
-	newsService.then(function(data) {
-		if (data.logout && data.logout == 'logout') {
-			window.location = "index.html#!/login";
-		}
+app.controller('RoomsController', ['$scope', '$sce', 'RoomsService', function ($scope, $sce, roomsService) {
+	function load() {
+		roomsService.then(function (data) {
+			if (data.logout && data.logout == 'logout') {
+				window.location = "index.html#!/login";
+			}
 
-		$scope.rooms = data.rooms;
-	});
+			$scope.rooms = data.rooms;
+		});
+		console.log('ok');
+	}
+
+	load();
+	setInterval(load, 5000);
 }]);
